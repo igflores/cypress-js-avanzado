d9ce2d049518d0bf1e304c91eb2414430aeb2891
igflores <139146133+igflores@users.noreply.github.com>
2024-06-27 10:38:53 -0500

    test(desafio-1): Added the test case

--- cypress/e2e/desafio-1.cy.js
+++ cypress/e2e/desafio-1.cy.js
@@ -2,7 +2,7 @@ import { registerPage } from "../support/pages/registerPage";
 import { loginPage } from "../support/pages/loginPage";
 import { welcomePage } from "../support/pages/welcomePage";
 import { productListPage } from "../support/pages/productListPage";
-import data from "../fixtures/data/desafio-1.json";
+import constants from "../support/constants";
 
 describe("Challenge # 1 - Online Shop: Add product, search for it, delete it, and search for it to verify that it does not exist", () => {
   let data;
@@ -23,8 +23,23 @@ describe("Challenge # 1 - Online Shop: Add product, search for it, delete it, an
   it("Add product, search for it, delete it, and search for it to verify that it does not exist", () => {
     productListPage.addProduct();
     productListPage.get.createProductPopup().should("be.visible");
-    productListPage.createNewProduct(data.name, data.price, data.url, data.id);
-
-    expect("1").to.be.equal("1");
+    productListPage.createNewProductSuccessfully(
+      data.name,
+      data.price,
+      data.url,
+      data.id
+    );
+    productListPage.closeMessageAlert();
+    productListPage.searchProductAdded(data.id);
+    productListPage.get
+      .productSearchedNameLabel({ timeout: constants.TIMEOUT * 3 })
+      .should("be.visible")
+      .and("have.text", data.name);
+    productListPage.deleteProductAddedSuccessfully();
+    productListPage.searchProductAdded(data.id);
+    productListPage.get
+      .productSearchedNameLabel({ timeout: constants.TIMEOUT })
+      .should("not.be.visible");
+    cy.get("img").should("not.be.visible");
   });
 });
--- cypress/support/pages/productListPage.js
+++ cypress/support/pages/productListPage.js
@@ -2,12 +2,6 @@ import constants from "../constants";
 
 class ProductListPage {
   get = {
-    searchBarInput: () => cy.get('input[data-cy="search-bar"]'),
-    messagealertModalHeader: () => cy.get('[id^="chakra-modal"] header'),
-    closeModalButton: () =>
-      cy.get('[id^="chakra-modal"] button[data-cy="closeModal"]'),
-    goToShoppingCartButton: () => cy.get('[data-cy="goShoppingCart"]'),
-
     createProductPopup: () =>
       cy.get('section[id^="chakra-modal"]', { timeout: constants.TIMEOUT }),
     addproductButton: () => cy.get('[data-cy="add-product"]'),
@@ -16,18 +10,44 @@ class ProductListPage {
     createProductUrlInput: () => cy.get('[data-cy="productCard"]'),
     createProductIdInput: () => cy.get('[data-cy="productID"]'),
     createProductButton: () => cy.get('[data-cy="createProduct"]'),
+    addedProductMessageCloseButton: () =>
+      cy.get('[data-cy="closeModal"]', { timeout: constants.TIMEOUT }),
+    searchTypeSelect: () => cy.get('[data-cy="search-type"]'),
+    searchBarInput: () =>
+      cy.get('[data-cy="search-bar"]', { timeout: constants.TIMEOUT }),
+    productSearchedNameLabel: () => cy.get('p[data-cy="name"]'),
+    deleteProductButton: () =>
+      cy.get('[data-cy^="delete"]', { timeout: constants.TIMEOUT }),
+    confirmDeleteProductButton: () =>
+      cy.get("#saveEdit", { timeout: constants.TIMEOUT }),
+    deletedProductMessageCloseButton: () => cy.get('[data-cy="closeModal"]'),
   };
 
   addProduct() {
     this.get.addproductButton().click();
   }
 
-  createNewProduct(name, price, url, id) {
+  createNewProductSuccessfully(name, price, url, id) {
     this.get.createProductNameInput().type(name);
     this.get.createProductPriceInput().type(price);
     this.get.createProductUrlInput().type(url);
     this.get.createProductIdInput().type(id);
     this.get.createProductButton().click();
+  }
+
+  closeMessageAlert() {
+    this.get.addedProductMessageCloseButton().click();
+  }
+
+  searchProductAdded(id) {
+    this.get.searchTypeSelect().select("ID");
+    this.get.searchBarInput().click().clear().type(id).type("{enter}");
+  }
+
+  deleteProductAddedSuccessfully() {
+    this.get.deleteProductButton().click({ timeout: constants.TIMEOUT });
+    this.get.confirmDeleteProductButton().click();
+    this.get.deletedProductMessageCloseButton().click();
   }
 }
 export const productListPage = new ProductListPage();
